f55263a5827c6687d9619bfdc4a3fb5799800bd3
masterhung0112 <masterhung0112@gmail.com>
2024-05-29 15:55:36 +0700

    Change router

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -107,9 +107,9 @@ const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
   // },
   // Fraxtal testnet
   [2522]: {
-    router: '0x935E5100ea3594AC65c50f9c2F3dFa6e49a56d21',
+    router: '0x1A62F683e0f72a622121aBdC8249781FA1C7D490',
     weth: '0xFC00000000000000000000000000000000000006',
-    creationBlock: 8185029,
+    creationBlock: 8185099,
   },
 }
 
